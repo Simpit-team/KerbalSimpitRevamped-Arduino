a0af52a8f09fa9a5a1daef148ecedd3c94ed242c
Peter Hardy <peter@hardy.dropbear.id.au>
2017-04-10 21:27:16 +1000

    Fix array derp.

--- Helpers.cpp
+++ Helpers.cpp
@@ -1,6 +1,8 @@
 #include "KerbalSimPit.h"
 
+const byte stageFlip[] = {0x01, 0x02};
+
 void KerbalSimPit::stageEvent()
 {
-  send(STAGE_PACKET, [0x01, 0x02], 2);
+  send(STAGE_PACKET, stageFlip, 2);
 }
