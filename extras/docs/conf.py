faf160b601c0f7fbdd68efb652627d77f39af771
Peter Hardy <peter@hardy.dropbear.id.au>
2017-05-16 22:21:35 +1000

    Add arduinodocs.

---
+++ extras/docs/conf.py
@@ -0,0 +1,33 @@
+import sys
+import os
+import shlex
+import subprocess
+
+read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+
+if read_the_docs_build:
+    subprocess.call('doxygen', shell=True)
+
+extensions = ['breathe']
+breathe_projects = { 'KerbalSimPit': 'xml' }
+breathe_default_project = "KerbalSimPit"
+templates_path = ['_templates']
+source_suffix = '.rst'
+master_doc = 'index'
+project = u'KerbalSimPit'
+copyright = u'2015, KerbalSimPit'
+author = u'KerbalSimPit'
+version = '1.0'
+release = '1.0'
+language = None
+exclude_patterns = ['_build']
+pygments_style = 'sphinx'
+todo_include_todos = False
+html_static_path = ['_static']
+htmlhelp_basename = 'KerbalSimPitdoc'
+latex_elements = {
+}
+latex_documents = [
+  (master_doc, 'KerbalSimPit.tex', u'KerbalSimPit Documentation',
+   u'KerbalSimPit', 'manual'),
+]
